95c65d154e0bbc4a846d33d39c2c747e58429a37
evaluato <evaluato@gmail.com>
2018-11-24 20:53:23 +0200

    Page rank problem - working variant

--- problems/page_rank_problem.py
+++ problems/page_rank_problem.py
@@ -1,15 +1,18 @@
+from constraints.b1_ball_surface import B1BallSurface
+from constraints.positive_simplex_surface import PositiveSimplexSurface
 from problems.viproblem import VIProblem
 import numpy as np
-from constraints.convex_set_constraint import ConvexSetConstraints
-from constraints.allspace import Rn
 from utils.print_utils import vectorToString
 
 
 class PageRankProblem(VIProblem):
     def __init__(self, GraphMatr: np.ndarray):
         self.A = GraphMatr
-        m = GraphMatr.shape[0]
-        super().__init__(x0=np.full(m, 1.0 / m))
+        self.m = GraphMatr.shape[0]
+        super().__init__(x0=np.concatenate((np.full(self.m, 1.0 / self.m), np.full(self.m, 1.0 / self.m))))
+
+        self.simplex = PositiveSimplexSurface(self.m,1)
+        self.sphere = B1BallSurface(self.m, 1)
 
         b = GraphMatr.sum(axis=0)[:, None]
         b[b == 0] = 1
@@ -43,12 +46,21 @@ class PageRankProblem(VIProblem):
 
     def load(self, basePath: str = 'data'):
         self.A = np.loadtxt("{0}/{1}_A.txt".format(basePath, self.__class__.__name__))
+        self.m = self.A.shape[0]
 
     def f(self, x: np.ndarray) -> float:
-        return np.linalg.norm(np.dot(self.A, x) - x)
+        return np.linalg.norm(np.dot(self.A, self.xpart(x)) - self.xpart(x))
+
+    def xpart(self, xy: np.ndarray):
+        return xy[:self.m]
+
+    def ypart(self, xy: np.ndarray):
+        return xy[self.m:]
 
     def df(self, x: np.ndarray) -> np.ndarray:
-        return np.dot(self.A, x) - x
+        return np.concatenate((self.A.T @ self.ypart(x) - self.ypart(x), -(self.A @ self.xpart(x) - self.xpart(x))))
+
+        #return (self.A - np.eye(self.A.shape[0], self.A.shape[0])) @ x
 
     def F(self, x: np.ndarray) -> float:
         return self.f(x)
@@ -57,7 +69,7 @@ class PageRankProblem(VIProblem):
         return self.df(x)
 
     def Project(self, x: np.ndarray) -> np.ndarray:
-        return x
+        return np.concatenate((self.simplex.project(self.xpart(x)), self.sphere.project(self.ypart(x))))
 
     def XToString(self, x: np.ndarray):
         return vectorToString(x)
--- saved_tests/pagerank-problem-tests.py
+++ saved_tests/pagerank-problem-tests.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-
+from methods.korpelevich import Korpelevich
 from problems.page_rank_problem import PageRankProblem
 
 # test from 25 bil eigenvector article. res = (0.38709677  0.12903226  0.29032258  0.19354839)
@@ -18,10 +18,21 @@ print("Page rank source matrix:\n", p.A)
 
 eValues, eVectors = np.linalg.eig(p.A)
 
+realAnswer = None
+
 for i, v in enumerate(eValues):
     if abs(v - 1)<0.00000001:
         v = abs(eVectors[:, i])
         v /= v.sum()
+        realAnswer = np.copy(v)
         print("Eigenvector: ", v)
         print("Test: ", np.linalg.norm(p.A@v - v))
 
+alg = Korpelevich(p, 0.00000001, 0.5, min_iters=10)
+
+for curState in alg:
+    print(alg.currentStateString())
+
+res = curState['x'][0][:p.m]
+print("Result: ", )
+print("Test  : ", np.linalg.norm(realAnswer - res))
